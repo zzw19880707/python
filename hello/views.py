5e0a2b776d3391f60aabd5a547abb99bc19180cc
作为 郑 <zzw414851474@qq.com>
2019-04-25 14:29:34 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -6,6 +6,9 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    import redis
+    conn = redis.Redis(host='redis-10684.c1.asia-northeast1-1.gce.cloud.redislabs.com',port=10684)
+    conn.set('k1','tttttt') # 向远程redis中写入了一个键值对
     return render(request, "index.html")
 
 
