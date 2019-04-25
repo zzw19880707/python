c67e74a275a7a8b233a32d7669c5c9e100d3d64e
作为 郑 <zzw414851474@qq.com>
2019-04-25 14:48:42 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import redis
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    import redis
-    conn = redis.Redis(host='redis-10684.c1.asia-northeast1-1.gce.cloud.redislabs.com',port=10684)
+
+    
+    conn = redis.Redis(host='redis-10684.c1.asia-northeast1-1.gce.cloud.redislabs.com',port=10684,password='VUnd8roSLg2v5R9pX6apCsODHbwdSdVG')
     conn.set('k1','tttttt') # 向远程redis中写入了一个键值对
     return render(request, "index.html")
 
@@ -20,3 +22,5 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
